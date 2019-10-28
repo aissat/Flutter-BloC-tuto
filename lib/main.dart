0447abdfc05f117512aae3ef7bd18057e0c35630
aissat <mr.aissat@gmail.com>
2019-10-28 22:23:21 +0100

    added  MultiBlocProvider and  home view

--- lib/main.dart
+++ lib/main.dart
@@ -1,28 +1,69 @@
 import 'package:bloc_counter/app_const.dart';
 import 'package:bloc_counter/counter/counter_bloc.dart';
-import 'package:bloc_counter/counter/counter_view.dart';
 import 'package:bloc_counter/router.dart';
+import 'package:bloc_counter/timer/bloc/bloc.dart';
+import 'package:bloc_counter/timer/ticker.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        primarySwatch: Colors.amber,
+    return MultiBlocProvider(
+        providers: <BlocProvider>[
+          BlocProvider<CounterBloc>(
+            builder: (context) => CounterBloc(),
+          ),
+          BlocProvider<TimerBloc>(
+            builder: (context) => TimerBloc(ticker: Ticker()),
+          ),
+        ],
+        child: MaterialApp(
+          title: 'Flutter Demo',
+          theme: ThemeData(
+            //primarySwatch: Colors.amber,
+            // primaryColor: Color.fromRGBO(109, 234, 255, 1),
+            // accentColor: Color.fromRGBO(72, 74, 126, 1),
+            brightness: Brightness.dark,
+          ),
+          home: HomeView(),
+          initialRoute: RoutePath.Home,
+          onGenerateRoute: Router.generateRoute,
+        ));
+  }
+}
+
+class HomeView extends StatelessWidget {
+  const HomeView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
       ),
-      home: BlocProvider(
-        builder: (context)=> CounterBloc(),
-        child: CounterView(),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              RaisedButton(
+                child: Text('Counte'),
+                onPressed: () => Navigator.pushNamed(context, RoutePath.Counter),
+              ),
+              RaisedButton(
+                child: Text('Timer'),
+                onPressed: () => Navigator.pushNamed(context, RoutePath.Timer),
+              )
+            ],
+          ),
+        ),
       ),
-      initialRoute: RoutePath.Home,
-      onGenerateRoute: Router.generateRoute,
     );
   }
 }
