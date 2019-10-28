813c87ed8216a5285987f928f93c251a4854fc90
aissat <mr.aissat@gmail.com>
2019-10-28 22:21:05 +0100

    added Timer in router

--- lib/router.dart
+++ lib/router.dart
@@ -1,12 +1,13 @@
 import 'package:bloc_counter/app_const.dart';
 import 'package:bloc_counter/counter/counter_view.dart';
+import 'package:bloc_counter/timer/timer_view.dart';
 import 'package:flutter/material.dart';
 
 class Router {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
-      case RoutePath.Home:
-        return MaterialPageRoute(builder: (_) => CounterView());
+      case RoutePath.Timer:
+        return MaterialPageRoute(builder: (_) => TimerView());
       case RoutePath.Counter:
         return MaterialPageRoute(builder: (_) => CounterView());
       default:
